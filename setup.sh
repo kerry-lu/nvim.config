59884d71f5f7e67c2e4450e2ae9dc1f1408a7c02
Kerry Lu <lukerry@amazon.com>
2024-01-19 01:41:06 +0000

    Replace lsp installs in setup.sh with MasonInstall

--- setup.sh
+++ setup.sh
@@ -39,31 +39,6 @@ elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt-get install ripgrep
 fi
 
-# lsp
-# python
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install npm
-    sudo npm i -g pyright
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    sudo apt-get install npm
-    sudo npm i -g pyright
-fi
-
-# c, cpp, objc, objcpp, cuda, proto
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install llvm
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    wget https://apt.llvm.org/llvm.sh
-    chmod +x llvm.sh
-    sudo ./llvm.sh 16
-    sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-16 100
-fi
-
 # packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim 2> /dev/null
-
-# cleanup
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    rm nvim.appimage llvm.sh
-fi
